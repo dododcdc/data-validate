e70a8af6d473530bcb1b0ba219acd0bc7d94ca46
benson <benson.zhong@farben.com.cn>
2021-01-20 15:09:40 +0800

    对比类优化

---
+++ data-check/src/main/java/com/farben/check/entity/ResSql.java
@@ -0,0 +1,57 @@
+package com.farben.check.entity;
+
+/**
+ * @Classname
+ * @Description
+ * @Date 2021/3/10 10:21
+ * @Author benson
+ **/
+public class ResSql {
+
+    /**
+     * 校验sql
+     */
+    private  String res;
+    /**
+     * 明细
+     */
+    private String detail;
+
+    /**
+     * 总条数
+     */
+    private String total;
+
+    public ResSql() {
+    }
+
+    public ResSql(String res, String detail) {
+        this.res = res;
+        this.detail = detail;
+    }
+
+    public String getRes() {
+        return res;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setRes(String res) {
+        this.res = res;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+}
+
--- data-check/src/main/java/com/farben/check/service/table/ComParm.java
+++ data-check/src/main/java/com/farben/check/service/table/ComParm.java
@@ -1,6 +1,8 @@
 package com.farben.check.service.table;
 
 
+import com.farben.check.entity.ResSql;
+
 import java.util.List;
 
 /**
@@ -18,7 +20,7 @@ public class ComParm {
     /**
      * 需要比较的字段名
      */
-    private List<String> fields ;
+    private List<String> fields;
 
     /**
      * 业务键
@@ -50,6 +52,7 @@ public class ComParm {
 
     /**
      * 生成表数据对比sql
+     *
      * @param comParm
      * @return
      */
@@ -60,20 +63,46 @@ public class ComParm {
         String tmp = "";
         for (int i = 0; i < comParm.fields.size(); i++) {
             tmp += ",SUM(CASE WHEN X." + this.fields.get(i) + " <> Y." + comParm.fields.get(i) +
-            " THEN 1 ELSE 0 END) AS " + this.fields.get(i) + " FROM " ;
+                    " THEN 1 ELSE 0 END) AS " + this.fields.get(i) + " FROM ";
         }
-        return  "";
+        return "";
     }
 
     /**
      * 生成表数据量对比sql
+     *
      * @param comParm
      * @return
      */
-    public String compCount(ComParm comParm) {
-        // todo 拼接数据量对比sql
+    public ResSql compCount(ComParm comParm) {
+        ResSql resSql = new ResSql();
+        StringBuilder str = new StringBuilder("SELECT  YC AS 原表数据量, MC AS 目标表数据量 FROM ( SELECT COUNT(*) AS YC FROM ");
+        str.append(this.dbName).append(".").append(this.tableName);
+        if (this.bizDate != null && !this.bizDate.equals("")) {
+            str.append(" WHERE ").append(this.bizDateName).append("='")
+                    .append(this.bizDate).append("'");
+        }
+        str.append(") X JOIN (SELECT COUNT(*) AS MC FROM ").append(comParm.dbName);
+        str.append(".").append(comParm.tableName);
+        if (comParm.bizDate != null && !comParm.bizDate.equals("")) {
+            str.append(" WHERE ").append(comParm.bizDateName).append("='")
+                    .append(comParm.bizDate).append("'");
+        }
+        str.append(") Y ON 1=1");
+        resSql.setDetail(str.toString());
 
-      return  "";
+        resSql.setRes(str.toString().replaceAll("YC AS 原表数据量, MC AS 目标表数据量", "YC - MC AS RES"));
+
+        String total = "SELECT COUNT(*) AS YC FROM " + this.dbName
+                + "." + this.tableName;
+        if (this.bizDate != null && !this.bizDate.equals("")) {
+            total = total + " WHERE " + this.bizDateName
+                    + "='" + this.bizDate + "'";
+        }
+        resSql.setTotal(total);
+
+        return resSql;
+
     }
 
 }

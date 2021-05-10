d04df372c9c9f8e74cb2bc200064c0ade1ccc231
dododcdc <dcdc3166@foxmail.com>
2021-05-10 11:21:11 +0800

    clojure

--- data-cl/src/foo/demo/g.clj
+++ data-cl/src/foo/demo/g.clj
@@ -2,6 +2,9 @@
 
 (def nums '(1 2 3))
 (println 'foo.demo.hello/stuName)
+(def arr (atom ["1 2 5" "b" "a"]))
+(swap! arr conj 1)
+(doseq [i @arr] (println i))
 (defn -main [& args]
   (def arr '(1 2 3))
   (println(reduce (fn [x y] (if (> x y) x y)) arr))

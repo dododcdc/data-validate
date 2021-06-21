9e7a86654595164905f8b99acd9279f731093cad
dododcdc <dcdc3166@foxmail.com>
2021-06-21 14:52:05 +0800

    加入tailwind

--- data-check-web/src/main.js
+++ data-check-web/src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import './index.css'
+
+
 import axios from 'axios'
 axios.defaults.baseURL = '/dev-api'
 axios.defaults.headers.post['Content-Type'] = 'application/json'

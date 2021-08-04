85644f0198b9b04d2cd69f8add798f7cf533a2f0
dododcdc <dcdc3166@foxmail.com>
2021-08-04 14:49:58 +0800

    update

---
+++ data-check/src/main/java/com/farben/check/prts/ForkJoinSumCalculator.java
@@ -0,0 +1,59 @@
+package com.farben.check.prts;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
+
+public class ForkJoinSumCalculator extends RecursiveTask<Long> {
+    private final long[] numbers;
+    private final int start;
+    private final int end;
+    public static final long THRESHOLD = 10_000;
+
+    public ForkJoinSumCalculator(long[] numbers) {
+        this(numbers,0,numbers.length);
+    }
+
+    public ForkJoinSumCalculator(long[] numbers, int start, int end) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end - start;
+        if (length < THRESHOLD) {
+            return computeSequentially(); // 如果大小小于阈值，顺序计算结果
+        }
+
+        ForkJoinSumCalculator leftfork = new ForkJoinSumCalculator(numbers, start, start + length / 2);
+        leftfork.fork();
+        ForkJoinSumCalculator rightfork = new ForkJoinSumCalculator(numbers, start + length / 2, end);
+        Long rightRes = rightfork.compute();
+        Long leftRes = leftfork.join();
+        return rightRes+leftRes;
+    }
+
+    private long computeSequentially() {
+        long sum = 0 ;
+        for (int i=start;i< end ;i++){
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public static long forkJoinSum(long n) {
+        long[] numbers = LongStream.rangeClosed(1,n).toArray();
+        ForkJoinTask<Long> task = new ForkJoinSumCalculator(numbers);
+        return new ForkJoinPool().invoke(task);
+    }
+
+    public static void main(String[] args) {
+
+        long l = ForkJoinSumCalculator.forkJoinSum(100000l);
+        System.out.println(l);
+    }
+
+}

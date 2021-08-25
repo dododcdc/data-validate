ad475222c653155ae93b6d6d28f438fe01c6878d
dododcdc <dcdc3166@foxmail.com>
2021-08-25 17:41:45 +0800

    update

--- data-check/src/main/java/com/farben/check/prts/sjjg/WbList.java
+++ data-check/src/main/java/com/farben/check/prts/sjjg/WbList.java
@@ -7,21 +7,23 @@ public class WbList {
 
     /**
      * 获取链表长度
+     *
      * @return
      */
     public int size() {
-        return this.length + 1 ;
+        return this.length;
     }
 
     /**
      * 添加一个元素
+     *
      * @param node
      */
     public void add(Node node) {
 
         if (this.head == null) {
             this.head = node;
-        }else {
+        } else {
 
             Node temp = this.head;
 
@@ -33,19 +35,68 @@ public class WbList {
 
         }
 
-        this.length ++;
+        ++this.length;
+    }
+
+    /**
+     * 根据下标查询
+     * @param index
+     * @return
+     */
+    public Node get(int index) {
+        if (index>length) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return  temp;
+    }
+
+    /**
+     * 移除一个元素
+     * @param node
+     */
+    public void remove(Node node) {
+        // 谁的下一个是node
+        Node pre = null;
+
+        // node 的下一个是谁
+        Node next = null;
+
+        Node temp = this.head;
+        for (int i = 0; i < this.length; i++) {
+
+            if (temp.next == node) {
+                pre = temp;
+                temp = temp.next;
+                continue;
+            }
+            if (node.next == temp) {
+                next = temp;
+                break;
+            }
+            temp = temp.next;
+        }
+
+        // 将node的上一个的next指向node的下一个
+        pre.next = next;
+        this.length--;
+
+
     }
 
     public static void main(String[] args) {
 
         WbList wbList = new WbList();
-        wbList.head = new Node("aa",null);
-
-        wbList.add(new Node("bb",null));
-        wbList.add(new Node("cc",null));
+        Node cc = new Node("cc", null);
+        wbList.add(new Node("bb", null));
+        wbList.add(cc);
+        wbList.add(new Node("dd", null));
 
         System.out.println(wbList.size());
-
+        System.out.println(wbList.get(2).data);
+        wbList.remove(cc);
+        System.out.println("over");
 
     }
 

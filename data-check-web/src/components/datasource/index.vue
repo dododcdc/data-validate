aff5c0c5ec3a25f49001beacfb95aa4171cb5ba0
dododcdc <dcdc3166@foxmail.com>
2022-01-19 14:52:17 +0800

    修改

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -131,7 +131,7 @@
                 </div>
               </div>
               <div class="-mr-2 flex md:hidden">
-                <!-- Mobile menu -->
+                <!-- Mobile  -->
                 <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                   <span class="sr-only">Open main menu</span>
                   <!--

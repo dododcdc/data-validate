15135c52c98829087677d53b7bdce91f4150a93c
dododcdc <dcdc3166@foxmail.com>
2022-01-26 15:57:20 +0800

    修改

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -202,7 +202,7 @@
             <div class="px-4 py-6 sm:px-0">
               <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
             </div>
-            <!-- /End replace -->
+
           </div>
         </main>
       </div>

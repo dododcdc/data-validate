40fd39ff8f81185be2462294ffd9eb4f9e424b9a
dododcdc <dcdc3166@foxmail.com>
2022-02-08 23:02:20 +0800

    下拉

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -110,7 +110,7 @@
                     </div>
 
                     <!--
-                      Dropdown menu, show/hide based on menu state.
+                       menu, show/hide based on menu state.
 
                       Entering: "transition ease-out duration-100"
                         From: "transform opacity-0 scale-95"

b12fea7cbdd1c122fe5cefdc7f3631f5c4659a56
dododcdc <dcdc3166@foxmail.com>
2022-02-12 23:03:35 +0800

    基础

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -110,7 +110,7 @@
                     </div>
 
                     <!--
-                        based on menu state.
+                        on menu state.
 
                       Entering: "transition ease-out duration-100"
                         From: "transform opacity-0 scale-95"

324e5da6c5e1ff6c710ab0fa1346df76b7bb5173
dododcdc <dcdc3166@foxmail.com>
2022-02-16 23:04:25 +0800

    状态

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -110,7 +110,7 @@
                     </div>
 
                     <!--
-                        on menu state.
+                        
 
                       Entering: "transition ease-out duration-100"
                         From: "transform opacity-0 scale-95"

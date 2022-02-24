160902fbb39dacb27b9148cba73b9285d9fdb6e2
dododcdc <dcdc3166@foxmail.com>
2022-02-24 23:05:57 +0800

    周期

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -112,7 +112,7 @@
                     <!--
                         
 
-                      :  duration-100"
+                      
                         From: "transform opacity-0 scale-95"
                         To: "transform opacity-100 scale-100"
                       Leaving: "transition ease-in duration-75"

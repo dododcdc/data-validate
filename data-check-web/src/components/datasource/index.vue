d2ee664ece12af587bd669dbb7c59d8d44835ff0
dododcdc <dcdc3166@foxmail.com>
2022-03-19 23:44:54 +0800

    更新

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -115,7 +115,7 @@
                       
                        
                         "  1"
-                      Leaving: "transition ease-in duration-75"
+                      Leaving: "trsition ease-in duration-75"
                         From: "transform opacity-100 scale-100"
                         To: " -0 scale-95"
                     -->

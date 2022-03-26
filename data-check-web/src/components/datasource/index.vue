97aeb8440b498318014118baf6077fa08f0d1dea
dododcdc <dcdc3166@foxmail.com>
2022-03-26 23:46:15 +0800

    更新

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -116,7 +116,8 @@
                        
                         "  1"
                       Leaving: 
-                        From: "transfom opacity-100 scale-100"
+                        From: "transfo
+                        m opacity-100 scale-100"
                         To: " -0 scale-95"
                     -->
                     <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">

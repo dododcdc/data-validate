a366a61ec9259c0dcbb603e92e7023e5184c3472
dododcdc <dcdc3166@foxmail.com>
2022-04-07 23:48:01 +0800

    更新

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -117,7 +117,7 @@
                         "  1"
                      
                         
-                        m opacity-100 scale-100"
+                        m opacity-10scale-100"
                         To: " -0 scale-95"
                     -->
                     <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">

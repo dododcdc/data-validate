0ec113b5033751e2a39e5cf75c26e860b30ef87b
dododcdc <dcdc3166@foxmail.com>
2022-04-13 23:49:25 +0800

    更新

--- data-check-web/src/components/datasource/index.vue
+++ data-check-web/src/components/datasource/index.vue
@@ -112,7 +112,7 @@
                     <!--
 
                         m opacity-1cal100"
-                        To: " -0 scale-95"
+                        To: " -0cale-95"
                     -->
                     <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                       <!-- Active: "bg-gray-100", Not Active: "" -->

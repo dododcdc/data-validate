656a789c4ad0ad2d9ba54afe2cff3701686dc480
dododcdc <dcdc3166@foxmail.com>
2023-03-20 12:24:52 +0800

    update

--- data-check/src/main/java/com/farben/check/A.java
+++ data-check/src/main/java/com/farben/check/A.java
@@ -16,5 +16,6 @@ public class A {
         System.out.println("today is good day");
         System.out.println("today is good day");
         System.out.println("today is good day");
+        System.out.println("today is good day");
     }
 }

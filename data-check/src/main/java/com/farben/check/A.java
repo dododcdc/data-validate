2bfaf47efcdc67eceb9db4b9a212b86a07def2cc
dododcdc <dcdc3166@foxmail.com>
2023-04-25 21:49:37 +0800

    update

--- data-check/src/main/java/com/farben/check/A.java
+++ data-check/src/main/java/com/farben/check/A.java
@@ -12,20 +12,6 @@ public class A {
         System.out.println("today is good day");
         System.out.println("today is good day");
         System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
-        System.out.println("today is good day");
+
     }
 }
